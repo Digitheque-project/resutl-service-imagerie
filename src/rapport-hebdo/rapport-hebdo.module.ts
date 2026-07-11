bbd228f45dc3396c53fce1cf09687a438a59fc45
Patrick Nir <nirimihaminapatrick@gmail.com>
2026-07-11 15:33:16 +0300

    fix: import Archive entity in RapportHebdoModule to resolve ArchiveRepository dependency

--- src/rapport-hebdo/rapport-hebdo.module.ts
+++ src/rapport-hebdo/rapport-hebdo.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RapportHebdo } from './rapport-hebdo.entity';
 import { RapportHebdoService } from './rapport-hebdo.service';
 import { RapportHebdoController } from './rapport-hebdo.controller';
+import { Archive } from '../archive/archive.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RapportHebdo])],
+  imports: [TypeOrmModule.forFeature([RapportHebdo, Archive])],
   providers: [RapportHebdoService],
   controllers: [RapportHebdoController],
   exports: [RapportHebdoService],
