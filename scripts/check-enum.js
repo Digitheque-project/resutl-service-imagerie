62bb6944496984ebaf47bc8660d663cbd7a6b89b
Patrick Nir <nirimihaminapatrick@gmail.com>
2026-07-11 11:24:52 +0300

    chore: remove temp scripts

--- scripts/check-enum.js
+++
@@ -1,6 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ host:'localhost', port:5432, user:'postgres', password:'Madagasikara', database:'result_db' });
-client.connect()
-  .then(() => client.query("SELECT enum_range(null::result_status_enum);"))
-  .then(r => { console.log(r.rows[0].enum_range); client.end(); })
-  .catch(e => { console.error(e.message); client.end(); });
